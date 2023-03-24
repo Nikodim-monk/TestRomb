6ab730db607836b1f2c4d49676cfb05c8f21ee11
Nikodim-monk <lr1975@yandex.ru>
2023-03-24 12:49:51 +0400

    2 вариант

--- src/Main.java
+++ src/Main.java
@@ -6,51 +6,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Задайте высоту ромба (1-80)");
         int height = scanner.nextInt();
         System.out.println("Задайте ширину ромба (1-80)");
         int width = scanner.nextInt();
-
         printDiamond(height, width);
     }
 
     private static void printDiamond(int height, int width) {
-        String[] line = new String[width];
-        line[width / 2] = "#";
-        print(line);
 
-        for (int y = 1; y < height / 2; y++) {
-            line = new String[width];
-            int index = (height / 2 - y) * width / height;
-            line[index] = "#";
-            line[width - index - 1] = "#";
-            print(line);
+        for (int y = 0; y < height; y++) {
+            String[] line = new String[width];
+            int index;
+            if (y < height / 2) {
+                index = (height / 2 - y) * width / height;
+                line[index] = "#";
+                line[width - index - 1] = "#";
+            } else if (y == height / 2) {
+                line[0] = "#";
+                line[width - 1] = "#";
+            } else {
+                index = (y - height / 2) * width / height;
+                line[index] = "#";
+                line[width - index - 1] = "#";
+            }
+            List<String> list = Arrays.asList(line);
+            list.replaceAll(t -> Objects.isNull(t) ? " " : t);
+            System.out.println(String.join("", list));
         }
-
-        line = new String[width];
-        line[0] = "#";
-        line[width - 1] = "#";
-        print(line);
-
-        for (int y = height / 2 - 1; y >= 1; y--) {
-            line = new String[width];
-            int index = (height / 2 - y) * width / height;
-            line[index] = "#";
-            line[width - index - 1] = "#";
-            print(line);
-        }
-
-        line = new String[width];
-        line[width / 2] = "#";
-        print(line);
-
     }
-
-    static void print(String[] arr) {
-        List<String> list = Arrays.asList(arr);
-        list.replaceAll(t -> Objects.isNull(t) ? " " : t);
-        System.out.println(String.join("", list));
-    }
-
 }
